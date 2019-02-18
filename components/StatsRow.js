9589844c036be7e33f205822ac51a8d5a024351b
yongelee <albertkim91@gmail.com>
2019-02-18 11:55:47 -0500

    Revert "added more context and check all working"
    This reverts commit a774cb8d7ada0ccdfd7cc6031af601caee6548ac.

--- components/StatsRow.js
+++ components/StatsRow.js
@@ -1,92 +1,90 @@
 import { useContext } from "react";
 import styled from "styled-components";
-import { useQuery } from "react-apollo-hooks";
+import { Query } from "react-apollo";
 
-import { FilterContext } from "../lib/filterContext";
+import { FilterContext } from "../lib/context";
 
 import { statsByFilter } from "../queries/filter/update";
-import { SelectedContext } from "../lib/selectedContext";
 
 const Wrapper = styled.div``;
 
 export default () => {
-  const { selectedState } = useContext(SelectedContext);
+  const { state } = useContext(FilterContext);
 
-  const { data, error, loading } = useQuery(statsByFilter, {
-    variables: {
-      data: {
-        filters: selectedState.statsSelected,
-        tourn_ids: []
-      }
-    },
-    fetchPolicy: "network-only"
-  });
+  // turn filters object into an array of stats that are true
+  const filters = Object.keys(state.filters)
+    .filter(key => state.filters[key])
+    .map(key => key);
 
-  if (loading) {
-    return (
-      <div className="stats-row">
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="stats-row">
-        <p>Error...</p>
-      </div>
-    );
-  }
-
-  console.log(data);
+  const tourn_ids = Object.keys(state.tourns)
+    .filter(key => state.tourns[key].checked)
+    .map(key => key);
 
   return (
-    <Wrapper className="stats-row">
-      {[1, 2, 3, 4, 5].map((x, i) => (
-        <div
-          key={i}
-          className="card"
-          style={{ padding: "1rem", margin: "1rem 0" }}
-        >
-          <h2 className="title">Most Kills</h2>
-          <table className="table is-fullwidth is-striped is-hoverable">
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1.</td>
-                <td>Miracle</td>
-                <td>83</td>
-              </tr>
-              <tr>
-                <td>2.</td>
-                <td>Sumail</td>
-                <td>33</td>
-              </tr>
-              <tr>
-                <td>3.</td>
-                <td>No[o]ne</td>
-                <td>33</td>
-              </tr>
-              <tr>
-                <td>4.</td>
-                <td>PPD</td>
-                <td>12</td>
-              </tr>
-              <tr>
-                <td>5.</td>
-                <td>Fear</td>
-                <td>11</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      ))}
-    </Wrapper>
+    <Query
+      query={statsByFilter}
+      variables={{
+        data: {
+          tourn_ids,
+          filters
+        }
+      }}
+      fetchPolicy="network-only"
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>loading...</p>;
+        if (error) return <p>error...</p>;
+        // console.log(data);
+        return (
+          <Wrapper className="stats-row">
+            {[1, 2, 3, 4, 5].map((x, i) => (
+              <div
+                key={i}
+                className="card"
+                style={{ padding: "1rem", margin: "1rem 0" }}
+              >
+                <h2 className="title">Most Kills</h2>
+                <table className="table is-fullwidth is-striped is-hoverable">
+                  <thead>
+                    <tr>
+                      <th>Rank</th>
+                      <th>Name</th>
+                      <th>Score</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Miracle</td>
+                      <td>83</td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Sumail</td>
+                      <td>33</td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>No[o]ne</td>
+                      <td>33</td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>PPD</td>
+                      <td>12</td>
+                    </tr>
+                    <tr>
+                      <td>5.</td>
+                      <td>Fear</td>
+                      <td>11</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            ))}
+          </Wrapper>
+        );
+      }}
+    </Query>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,19 @@
 import App, { Container } from "next/app";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
-import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-
 import withApollo from "../lib/withApollo";
-import { FilterContextProvider } from "../lib/filterContext";
-import { SelectedContextProvider } from "../lib/selectedContext";
+import { FilterContextProvider } from "../lib/context";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
-          <ApolloHooksProvider client={apolloClient}>
-            <FilterContextProvider>
-              <SelectedContextProvider>
-                <Component {...pageProps} />
-              </SelectedContextProvider>
-            </FilterContextProvider>
-          </ApolloHooksProvider>
-        </ApolloProvider>
+        <FilterContextProvider>
+          <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </FilterContextProvider>
       </Container>
     );
   }
