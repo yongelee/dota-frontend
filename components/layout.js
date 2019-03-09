5248a14d186c8fe5c53f2627fa6f89cbb47d4fcd
yongelee <albertkim91@gmail.com>
2019-03-09 17:19:27 -0500

    updated head title

--- components/layout.js
+++ components/layout.js
@@ -17,7 +17,9 @@ const Inner = styled.div``;
 export default ({ children }) => (
   <div>
     <Head>
-      <title>Swag report</title>
+      <title>
+        Road to 4k | Search for top performers at Dota 2 tournaments!
+      </title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
